cf06131921d8952dbb1119e3b0bdfc12d63710aa
Renan Martins <renanrodm@gmail.com>
2025-09-08 16:19:21 -0300

    Adicionado rota GET /helloworld para retornar um hello world em HTML. Adicionado titulo a aplicação, primeiro schema Pydantic para a rota GET /read_root e explicitando modelos de respostas das rota. Adicionado teste unitário para rota GET /helloworld

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,18 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fast_zero.schemas import Message
+
+app = FastAPI(title='Fast Zero')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, Mundo!'}
+
+
+@app.get('/helloworld', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello_world():
+    return """<html><h1>Olá, Mundo!</h1></html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -21,3 +21,12 @@ def test_root_deve_retornar_ola_mundo():
     # Assert
     assert response.json() == {'message': 'Olá, Mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_hello_world_deve_retornar_ola_mundo_html():
+    client = TestClient(app)
+
+    response = client.get('/helloworld')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '<html><h1>Olá, Mundo!</h1></html>'
