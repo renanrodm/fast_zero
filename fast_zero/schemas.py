a4de4a48849998d0773fe201f167d8a9bd5bc802
Renan Martins <renanrodm@gmail.com>
2025-09-09 08:22:03 -0300

    Implementado rota CRUD para usuários; Adicionado testes unitários para os cenários possíveis;

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,5 +1,30 @@
-from pydantic import BaseModel
+from typing import List
+
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserList(BaseModel):
+    users: List[UserPublic]
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,13 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ola_mundo():
+def test_root_deve_retornar_ola_mundo(client):
     """
     Teste em 3 etapas (AAA)
     - #Arrange: Arranjo/Organizar
     - #Act: Executa a coisa (o SUI)
     - #Assert: Garanta que A é A
     """
-    # Arrange
-    client = TestClient(app)
-
     # Act
     response = client.get('/')
 
@@ -23,10 +16,91 @@ def test_root_deve_retornar_ola_mundo():
     assert response.status_code == HTTPStatus.OK
 
 
-def test_hello_world_deve_retornar_ola_mundo_html():
-    client = TestClient(app)
-
+def test_hello_world_deve_retornar_ola_mundo_html(client):
     response = client.get('/helloworld')
 
     assert response.status_code == HTTPStatus.OK
     assert response.text == '<html><h1>Olá, Mundo!</h1></html>'
+
+
+def test_create_user(client):
+    # Enviamos com o User, schema de entrada
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Renan',
+            'password': '12345',
+            'email': 'renan@email.com',
+        },
+    )
+
+    # Validar o UserPublic, schema de saída.
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'Renan',
+        'email': 'renan@email.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'Renan',
+                'email': 'renan@email.com',
+                'id': 1,
+            }
+        ]
+    }
+    # [MÁ PRÁTICA]: Esse teste depende do teste de cima para validar os dados.
+    # O motivo disso é por não termos o banco dados real implementado. Ainda...
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'Renan',
+            'password': '12345',
+            'email': 'renan@email.com',
+            'id': 1,
+        },
+    )
+
+    assert response.json() == {
+        'username': 'Renan',
+        'email': 'renan@email.com',
+        'id': 1,
+    }
+
+
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'Renan',
+            'password': '12345',
+            'email': 'renan@email.com',
+            'id': 999,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
